423cb2c93019134e174553b84867d6af75921bd3
Mark Pavlyuk <mmarikk.violinist1@gmail.com>
2022-10-26 23:08:26 -0700

    Added Contact Me page

--- app.js
+++ app.js
@@ -22,5 +22,5 @@ $("a:contains('About')").click(function(){
 });
 
 $("a:contains('Contact')").click(function(){
-  alert("Contact me section is coming up soon. Please, be patient!");
+  //alert("Contact me section is coming up soon. Please, be patient!");
 });
